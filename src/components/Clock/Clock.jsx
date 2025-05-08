f117d57d26a3c1dc1a24957bef71dc7f02d59cba
Norbert Kovács <k.n.89@outlook.hu>
2025-05-08 18:47:22 +0200

    Clock func

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../../styles/components/Clock/Clock.scss'
 
 export const Clock = () => {
+  const [currentTime, setCurrentTime] = useState(new Date())
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date())
+    }, 1000) // update every 1000 milliseconds (1 second)
+
+    return () => clearInterval(timer)
+  }, [])
+
+  const formatTime = (date) => {
+    const hours = date.getHours().toString().padStart(2, '0')
+    const minutes = date.getMinutes().toString().padStart(2, '0')
+    const seconds = date.getSeconds().toString().padStart(2, '0')
+
+    return `${hours}:${minutes}:${seconds}`
+  }
+
   return (
     <div className='clock_container'>
       <div className='inner_container'>
-        <div className='actual_clock'>Clock</div>
+        <div className='actual_clock'>{formatTime(currentTime)}</div>
       </div>
     </div>
   )
